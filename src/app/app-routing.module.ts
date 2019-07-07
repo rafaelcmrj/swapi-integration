66b7f16986dd2c826c46a674a10ce00b59d46df3
Rafael Moreira <rafaelmoreira@Rafaels-MacBook-Pro.local>
2019-07-07 12:28:51 -0600

    added file structure for people component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PeopleComponent } from './people/people.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/people', pathMatch: 'full' },
+  { path: 'people', component: PeopleComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
