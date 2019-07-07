aedcccc4f7f542ae9eff996b68c14e513821ed28
Rafael Moreira <rafaelmoreira@Rafaels-MacBook-Pro.local>
2019-07-07 13:34:24 -0600

    added angular material dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PeopleComponent } from './people/people.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,8 @@ import { PeopleComponent } from './people/people.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule,
     AppRoutingModule
   ],
   providers: [],
